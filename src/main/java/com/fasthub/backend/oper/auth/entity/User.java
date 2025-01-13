0dcc438dad18777158cfb074a03b35d454b6c862
DESKTOP-CB5R3AI\USER <wjdtjr9401@naver.com>
2025-01-13 16:52:31 +0900

    add auto login jwt

--- src/main/java/com/fasthub/backend/oper/auth/entity/User.java
+++ src/main/java/com/fasthub/backend/oper/auth/entity/User.java
@@ -5,12 +5,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
-import java.util.*;
 
 @Getter
 @Builder
--- src/main/java/com/fasthub/backend/oper/auth/service/CoustomUserDetailService.java
+++ src/main/java/com/fasthub/backend/oper/auth/service/CoustomUserDetailService.java
@@ -5,6 +5,8 @@ import com.fasthub.backend.oper.auth.dto.UserDto;
 import com.fasthub.backend.oper.auth.entity.User;
 import com.fasthub.backend.oper.auth.repository.AuthRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 @Service
 @Transactional(readOnly = true)
+@Slf4j
 public class CoustomUserDetailService implements UserDetailsService {
 
     private final AuthRepository authRepository;
@@ -24,6 +27,8 @@ public class CoustomUserDetailService implements UserDetailsService {
     public CustomUserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
         User user = authRepository.findById(Long.valueOf(userId))
                 .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저가 없습니다."));
+
+       log.info("customUserDetailArea : " + user.toString());
 
         UserDto dto = modelMapper.map(user, UserDto.class);
 
