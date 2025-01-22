48e0fe9918a56b8b831a235906f07e9604a77c63
DESKTOP-CB5R3AI\USER <wjdtjr9401@naver.com>
2025-01-22 16:56:50 +0900

    add brand folder

---
+++ src/main/java/com/fasthub/backend/oper/brand/contoller/BrandContoller.java
@@ -0,0 +1,9 @@
+package com.fasthub.backend.oper.brand.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/brand")
+public class BrandContoller {
+}
