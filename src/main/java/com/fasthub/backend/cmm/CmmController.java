94f188132db94f3654eb3973de85fd5ab4bf6fad
차정석 <wjdtjr9401@naver.com>
2025-10-28 22:45:01 +0900

    #params 생성 취소 및 boot 자동 바인딩을 위해 변환 과정 진행중

--- src/main/java/com/fasthub/backend/cmm/CmmController.java
+++ src/main/java/com/fasthub/backend/cmm/CmmController.java
@@ -1,6 +1,8 @@
 package com.fasthub.backend.cmm;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasthub.backend.cmm.result.Params;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,33 +28,38 @@ public class CmmController {
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @RequestMapping("/*/*")
     public ResponseEntity<?> handleAllRequests(
             HttpServletRequest request, @RequestBody(required = false) String body) {
         String path = request.getRequestURI();
         String[] pathList = path.split("/");
-
         String className = getClassName(pathList[2]);
         String methodName = pathList[3];
-        Params params = setinngRequestParams(request);
-
-        if (pathList[1].equals("api"))  handleApiRequest(className, methodName, params);
+        log.info("body : " + body);
+        if (pathList[1].equals("api"))  handleApiRequest(className, methodName, body);
         return ResponseEntity.ok("Request processed");
     }
 
-    public String getClassName(String className){
-        return "com.fasthub.backend.oper."+className+".service."+capitalize(className)+"Service";
-    }
 
-    public Object handleApiRequest(String className, String methodName, Object... args){
+
+    public Object handleApiRequest(String className, String methodName, String jsonbody){
         Class<?> clazz = null;
         try {
             clazz = Class.forName(className);
+            log.info("[실행 class : " + clazz.getName() + "]");
             Object instance = applicationContext.getBean(clazz);
-            Class<?> paramsClass = Class.forName("com.fasthub.backend.cmm.result.Params");
-            Method method = clazz.getDeclaredMethod(methodName, paramsClass);
+            log.info("[실행 instance :"+ instance + "]");
+            Method method = clazz.getDeclaredMethod(methodName);
+            log.info("[실행 method : " + method + "]");
+
+            log.info("jsonBody : " + jsonbody);
+
+
             method.setAccessible(true);
-            return method.invoke(instance, args);
+            return method.invoke(instance);
         } catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException |
                  IllegalAccessException e) {
             log.error("search error class and method");
@@ -58,25 +67,47 @@ public class CmmController {
         }
     }
 
-    public Params setinngRequestParams(HttpServletRequest request){
-        Params params = new Params();
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
-            String key = entry.getKey();
-            String[] values = entry.getValue();
-            log.info("key : " + key);
-            log.info("vlaue : " + values);
-            params.add(key, values);
+    public Method findMethod(Class<?> serviceClass, String methodName, String jsonBody) throws NoSuchMethodException, JsonProcessingException {
+        List<Method> candidates = new ArrayList<>();
+        for (Method m : serviceClass.getDeclaredMethods()) {
+            if (m.getName().equals(methodName)) {
+                candidates.add(m);
+            }
         }
-        return params;
+        // 하나뿐이면 바로 반환
+        if (candidates.size() == 1) return candidates.get(0);
+
+        Map<String, Object> jsonMap = objectMapper.readValue(jsonBody, Map.class);
+
+        candidates.forEach(item -> {
+            log.info("item : " + item);
+        });
+
+        return null;
+    }
+
+    public boolean canMatchMethod(Method method, Map<String, Object> jsonMap){
+        Parameter[] params = method.getParameters();
+
     }
 
     /**
-     * 첫 글자만 대문자로 변환
+     * 첫 글자만 대문자로 반환
+     * @param str
+     * @return
      */
     public static String capitalize(String str) {
         if (str == null || str.isEmpty()) {return str;}
         return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    /**
+     * class path 반환
+     * @param className
+     * @return
+     */
+    public String getClassName(String className){
+        return "com.fasthub.backend.oper."+className+".service."+capitalize(className)+"Service";
     }
 
 
