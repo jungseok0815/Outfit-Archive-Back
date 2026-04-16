765228eda8b369516b74747660cdabebaa225ad9
DESKTOP-CB5R3AI\USER <wjdtjr9401@naver.com>
2026-04-16 09:28:37 +0900

    test product collect

--- src/test/java/com/fasthub/backend/cmm/naver/NaverProductCollectServiceTest.java
+++ src/test/java/com/fasthub/backend/cmm/naver/NaverProductCollectServiceTest.java
@@ -2,13 +2,28 @@ package com.fasthub.backend.cmm.naver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "jasypt.encryptor.password=")
 class NaverProductCollectServiceTest {
 
     @Autowired
     private NaverProductCollectService naverProductCollectService;
+
+    @Value("${naver.api.client-id}")
+    private String clientId;
+
+    @Value("${naver.api.client-secret}")
+    private String clientSecret;
+
+    @Test
+    void checkNaverKeys() {
+        System.out.println("clientId: " + clientId);
+        System.out.println("clientSecret: " + clientSecret);
+    }
 
     @Test
     void collectTest() {
